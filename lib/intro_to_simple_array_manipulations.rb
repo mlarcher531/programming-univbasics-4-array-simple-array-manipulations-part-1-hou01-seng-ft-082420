ad1cd7e8c294cb676641014b573607f41f2b8492
Mike Archer <mlarcher531@gmail.com>
2020-07-25 21:15:13 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+def using_push(array, string)
+  colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  colors_in_the_rainbow.push("violet")
+end
+
+def using_unshift(array, string)
+    bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+    bouroughs_in_nyc.unshift("Staten Island")
+  end
+
+  def using_pop(array)
+    array.pop
+  end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
