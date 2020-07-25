a9a93d00ff0ee160eca82988d4d0a46b1e2f2a5f
Mike Archer <mlarcher531@gmail.com>
2020-07-25 20:46:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,13 +2,3 @@ def using_push(array, string)
   colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
   colors_in_the_rainbow.push("violet")
 end
-
-def using_unshift(array, string)
-  array.unshift(string)
-end
-
-def using_pop(array, element)
-continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  continents.pop("Antarctica")
-
-end
